9de1b910e988e0d54b5da2e9602d3f4ee743d058
Cleber Ferrari <cleber.ferrari@gmail.com>
2021-11-02 08:38:30 -0300

    ajuste layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,27 +8,28 @@ import DonutChart from "components/DonutChart";
 function App() {
   return (
     <>
-      
+
       <NavBar />
       <div className="container">
         <h1 className="text-primary">DashBoard de vendas - by Cleber Ferrari</h1>
 
-        <div className="row px-3" />
-        <div className="col-sm-6">
-          <h5 className="text-center text-secondary">taxa de sucesso (%)</h5>
-          <BarChart />
-        </div>
-        <div className="col-sm-6">
-        <h5 className="text-center text-secondary">todas vendas</h5>
-          <DonutChart />
+        <div className="row px-3" >
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">todas vendas</h5>
+            <DonutChart />
+          </div>
+
+          <div className="py-3">
+            <h2 className="text-primary">Todas vendas</h2>
+          </div>
+
+          <DataTable />
         </div>
       </div>
-      <div className="py-3">
-        <h2 className="text-primary">Todas vendas</h2>
-      </div>
-
-      <DataTable />
-
       <Footer /></>
 
   );
