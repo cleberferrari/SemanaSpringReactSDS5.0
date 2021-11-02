45558d6d7bc398f610924fba65f0b3c73f782a09
Cleber Ferrari <cleber.ferrari@gmail.com>
2021-11-02 00:34:15 -0300

    gráficos estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,36 @@
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 
 
 function App() {
   return (
     <>
+      
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo - by Cleber Ferrari</h1>
-        <DataTable />
+        <h1 className="text-primary">DashBoard de vendas - by Cleber Ferrari</h1>
+
+        <div className="row px-3" />
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">taxa de sucesso (%)</h5>
+          <BarChart />
+        </div>
+        <div className="col-sm-6">
+        <h5 className="text-center text-secondary">todas vendas</h5>
+          <DonutChart />
+        </div>
       </div>
-      <Footer />
-    </>
+      <div className="py-3">
+        <h2 className="text-primary">Todas vendas</h2>
+      </div>
+
+      <DataTable />
+
+      <Footer /></>
+
   );
 }
 
